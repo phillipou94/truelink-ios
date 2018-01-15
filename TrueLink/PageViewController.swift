1337783b75cd7bf41451dbc1836e1160647f9081
Phillip Ou <pou@mit.edu>
2018-01-15 20:39:17 +0800

    first commit.

---
+++ TrueLink/PageViewController.swift
@@ -0,0 +1,82 @@
+//
+//  PageViewController.swift
+//  TrueLink
+//
+//  Created by Phillip Ou on 1/15/18.
+//  Copyright © 2018 Phillip Ou. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class PageViewController: UIViewController, UIPageViewControllerDataSource {
+    var viewControllers : [UIViewController] = []
+    var pageViewController: UIPageViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Checking!");
+        
+        self.pageViewController = UIPageViewController(transitionStyle: UIPageViewControllerTransitionStyle.scroll, navigationOrientation: UIPageViewControllerNavigationOrientation.horizontal, options: nil)
+        
+        let initialViewController = self.viewControllers[0]
+        let startViewControllers:[UIViewController] = [initialViewController]
+        
+        
+        self.pageViewController.setViewControllers(startViewControllers, direction: .forward, animated: true, completion: nil)
+        self.pageViewController.dataSource = self
+        
+        self.addChildViewController(self.pageViewController)
+        self.pageViewController.view.frame = self.view.frame
+        self.view.addSubview(self.pageViewController.view)
+        
+        self.pageViewController.didMove(toParentViewController: self)
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: data source
+    
+    
+    
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        return nil
+    }
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        
+        return nil
+    }
+    
+    
+    
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        
+        return 0
+        
+    }
+    
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        
+        return 0
+        
+    }
+    
+
+
+}
---
+++ TrueLink/UIColor+TLColors.swift
@@ -0,0 +1,64 @@
+//
+//  UIColor+TLColors.swift
+//  TrueLink
+//
+//  Created by Phillip Ou on 1/15/18.
+//  Copyright © 2018 Phillip Ou. All rights reserved.
+//
+
+//
+//  UIColor+ImpactColors.swift
+//  Impact
+//
+//  Created by Phillip Ou on 8/20/15.
+//  Copyright (c) 2015 Impact. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIColor {
+    
+    static func TLBlack() -> UIColor {
+        return TLBlackWithAlpha(alpha: 1.0)
+    }
+    
+    static func TLOrange() -> UIColor {
+        return TLOrangeWithAlpha(alpha: 1.0)
+    }
+    
+    static func TLDarkGrey() -> UIColor {
+        return TLDarkGreyWithAlpha(alpha: 1.0)
+    }
+    
+    static func TLLightGrey() -> UIColor {
+        return TLLightGreyWithAlpha(alpha: 1.0)
+    }
+    
+    static func TLOffWhite() -> UIColor {
+        return TLOffWhiteWithAlpha(alpha: 1.0)
+    }
+    
+    static func TLBlackWithAlpha(alpha:CGFloat) -> UIColor {
+        return UIColor(red: 74/255.0, green: 74/255.0, blue: 74/255.0, alpha: alpha);
+    }
+    
+    static func TLOrangeWithAlpha(alpha:CGFloat) -> UIColor {
+        return UIColor(red: 242/255.0, green: 107/255.0, blue: 92/255.0, alpha: alpha);
+    }
+    
+    static func TLDarkGreyWithAlpha(alpha:CGFloat) -> UIColor {
+        return UIColor(red: 196/255.0, green: 191/255.0, blue: 191/255.0, alpha: alpha);
+    }
+    
+    static func TLLightGreyWithAlpha(alpha:CGFloat) -> UIColor {
+        return UIColor(red: 196/255.0, green: 191/255.0, blue: 191/255.0, alpha: alpha);
+    }
+    
+    static func TLOffWhiteWithAlpha(alpha:CGFloat) -> UIColor {
+        return UIColor(red: 250/255.0, green: 248/255.0, blue: 248/255.0, alpha: alpha);
+    }
+
+    
+}
+
