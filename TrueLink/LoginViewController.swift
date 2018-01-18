cd0c055d20017d7eabb15c5c7f3d007c7f2128dc
Hanna Lee <hannalee995@gmail.com>
2018-01-19 00:18:32 +0800

    sending itay will trigger beacon light on

--- TrueLink/LoginViewController.swift
+++ TrueLink/LoginViewController.swift
@@ -10,102 +10,7 @@ import UIKit
 import Bluejay
 import CoreBluetooth
 
-class LoginViewController: UIViewController, UITextFieldDelegate, CBCentralManagerDelegate, CBPeripheralDelegate {
-
-    var centralManager: CBCentralManager!
-    var arduinoPeripheral: CBPeripheral!
-    
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        switch central.state {
-            
-        case .unknown:
-            print("central.state is .unknown")
-        case .resetting:
-            print("central.state is .resetting")
-        case .unsupported:
-            print("central.state is .unsupported")
-
-        case .unauthorized:
-            print("central.state is .unauthorized")
-
-        case .poweredOff:
-            print("central.state is .poweredOff")
-
-        case .poweredOn:
-            print("central.state is .poweredOn")
-            let serviceIdentifier = "19B10000-E8F2-537E-4F6C-D104768A1214"
-            centralManager.scanForPeripherals(withServices: [CBUUID(string: serviceIdentifier)])
-        }
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        print("Found peripheral: ", peripheral)
-        arduinoPeripheral = peripheral
-        arduinoPeripheral.delegate = self
-        centralManager.stopScan()
-        centralManager.connect(arduinoPeripheral)
-    }
-    
-    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        print("Connected to peripheral: ", peripheral)
-        let serviceIdentifier = "19B10000-E8F2-537E-4F6C-D104768A1214"
-        arduinoPeripheral.discoverServices([CBUUID(string: serviceIdentifier)])
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        guard let services = peripheral.services else {return}
-        
-        let arduinoService = services[0]
-        let characteristicIdentifier = "19B10001-E8F2-537E-4F6C-D104768A1214"
-        peripheral.discoverCharacteristics([CBUUID(string: characteristicIdentifier)], for: arduinoService)
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        guard let characteristics = service.characteristics else {return}
-        
-        let arduinoCharacteristic = characteristics[0]
-        print(arduinoCharacteristic)
-        
-        if arduinoCharacteristic.properties.contains(.read) {
-            print("properties contains .read")
-        }
-        if arduinoCharacteristic.properties.contains(.notify) {
-            print("properties contains .notify")
-        }
-        
-        peripheral.setNotifyValue(true, for: arduinoCharacteristic)
-        print("writeValue a")
-        peripheral.writeValue("a".data(using: .utf8)!, for: arduinoCharacteristic, type: .withResponse)
-        print("writeValue b")
-        peripheral.writeValue("b".data(using: .utf8)!, for: arduinoCharacteristic, type: .withResponse)
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
-        if error != nil {
-            print("error writing value")
-        } else {
-//            print("readValue")
-//            peripheral.readValue(for: characteristic)
-        }
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didUpdateNotificationStateFor characteristic: CBCharacteristic, error: Error?) {
-        print("didUpdateNotificationStateFor")
-        if error != nil {
-            print("uh oh", error)
-        }
-    }
-    
-    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        print("didUpdateValueFor")
-        let newValue = String(data: characteristic.value!, encoding: String.Encoding.ascii)
-        print(newValue)
-//        if newValue == "c" {
-//            print("send love to beacon 1")
-//        } else if newValue == "d" {
-//            print("send love to beacon 2")
-//        }
-    }
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -116,10 +21,6 @@ class LoginViewController: UIViewController, UITextFieldDelegate, CBCentralManag
         self.initTextFields()
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
         self.view.addGestureRecognizer(tapGesture)
-        
-        // Do any additional setup after loading the view.
-        centralManager = CBCentralManager(delegate: self, queue: nil)
-        
     }
 
     override func didReceiveMemoryWarning() {
