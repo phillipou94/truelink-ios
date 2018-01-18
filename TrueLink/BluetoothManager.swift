4b4d366cc3b2f63c228b505b61a53611b325b093
Hanna Lee <hannalee995@gmail.com>
2018-01-18 12:27:20 +0800

    WIP bluetooth, rip pod install

---
+++ TrueLink/BluetoothManager.swift
@@ -0,0 +1,13 @@
+//
+//  BluetoothManager.swift
+//  TrueLink
+//
+//  Created by Hanna Lee on 1/18/18.
+//  Copyright © 2018 Phillip Ou. All rights reserved.
+//
+
+import UIKit
+
+class BluetoothManager: NSObject {
+
+}
